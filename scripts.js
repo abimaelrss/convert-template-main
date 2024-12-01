73fa01c632d0740ca7fe6fdbd05f290beb8d3ed0
Abimael <abimaelrss@gmail.com>
2024-12-01 09:42:37 -0300

    Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dia. 01/12/2024
+const USD = 5.97;
+const EUR = 6.34;
+const GBP = 7.6;
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
@@ -8,11 +13,29 @@ amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
 
   amount.value = amount.value.replace(hasCharactersRegex, "");
-})
+});
 
 // Captando o envento de submit (enviar) do formulário.
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log(currency.value);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(currency.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(currency.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(currency.value, GBP, "£");
+      break;
+
+    default:
+      break;
+  }
+};
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
 }
