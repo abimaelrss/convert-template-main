0ff78ccb368acbc6a7dff9bdfd66f522041db80f
Abimael <abimaelrss@gmail.com>
2024-12-01 09:52:02 -0300

    Exibindo a cotação da moeda

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 // Cotação de moedas do dia. 01/12/2024
 const USD = 5.97;
 const EUR = 6.34;
-const GBP = 7.6;
+const GBP = 7.60;
 
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -39,6 +40,8 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${price}`;
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result");
   } catch (error) {
